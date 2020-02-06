7d4eb9eb6e922926840e5f2b628998442237564f
Christopher  Lara <christopher@MacBook-Air-de-Christopher.local>
2020-02-06 01:00:48 -0300

    [Global] initial commit

---
+++ music/music/src/api/config.js
@@ -0,0 +1,11 @@
+//Application name	music
+//API key	bdd6adee3fee68c27317757c1ac9b036
+//Shared secret	c4a7d019f69fbb491d5c40df4a71e10c
+//Registered to	Chris_lara
+
+export default {
+    appName: 'music',
+    apiKey: 'bdd6adee3fee68c27317757c1ac9b036',
+    secret: 'c4a7d019f69fbb491d5c40df4a71e10c',
+    RegisteredTo: 'Chris_lara'
+}
